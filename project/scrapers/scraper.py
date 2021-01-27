13466c4408806537343409f9748641171dcfce53
Michael Herman <hermanmu@gmail.com>
2021-01-26 20:11:44 -0600

    Update scraper.py

--- project/scrapers/scraper.py
+++ project/scrapers/scraper.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from bs4 import BeautifulSoup
 
 
