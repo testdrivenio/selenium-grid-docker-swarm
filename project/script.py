8324ca6a5a3de9cba2579446f8901d4a2fd3b8f7
Michael Herman <hermanmu@gmail.com>
2022-03-29 08:21:13 -0500

    update base

--- project/script.py
+++ project/script.py
@@ -4,18 +4,21 @@ from time import sleep
 from scrapers.scraper import get_driver, connect_to_base, parse_html
 
 
-def run_process(browser, page_number=1):
-    if connect_to_base(browser, page_number):
-        print(f'Scraping page {page_number}...')
+
+def run_process(rowser):
+    if connect_to_base(browser):
+        print(f'Scraping random Wikipedia page...')
         sleep(2)
         html = browser.page_source
         return parse_html(html)
     else:
+        print("Error connecting to Wikipedia")
         return False
 
 
 if __name__ == '__main__':
     browser = get_driver()
-    data = run_process(browser, sys.argv[1])
+    data = run_process(browser)
+    print(data)
     browser.quit()
-    print(f'Finished page {sys.argv[1]}')
+    print(f'Finished!')
